847bda0e96ce39e3f91385a365886662298a6e72
Victor Grau Serrat <lacabra@users.noreply.github.com>
2019-01-29 11:02:11 -0600

    BLD: HW mode

--- enigma-core/start_core.bash
+++ enigma-core/start_core.bash
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd /root/enigma-core/enigma-core/app
-# sed -i '/\#\[ignore\]/d' src/networking/ipc_listener.rs
-# . /opt/sgxsdk/environment && . /root/.cargo/env && SGX_MODE=SW cargo test test_real_listener
-. /opt/sgxsdk/environment && . /root/.cargo/env && SGX_MODE=SW cargo build
+. /opt/sgxsdk/environment && . /root/.cargo/env && cargo build
 ./target/release/enigma-core-app
