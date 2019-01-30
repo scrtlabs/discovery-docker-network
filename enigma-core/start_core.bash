e75aad21901847e02258faf0c56fd50af8b37f4c
Victor Grau Serrat <lacabra@users.noreply.github.com>
2019-01-29 18:31:17 -0600

    BLD: HW mode fix

--- enigma-core/start_core.bash
+++ enigma-core/start_core.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+/opt/intel/libsgx-enclave-common/aesm/aesm_service &
+sleep 5 # give time to aesm_service to start
 cd /root/enigma-core/enigma-core/app
-. /opt/sgxsdk/environment && . /root/.cargo/env && cargo build
 ./target/release/enigma-core-app
