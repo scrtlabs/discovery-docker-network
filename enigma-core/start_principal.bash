f8569abb2666b79e04c6d231807b200bdb19c1f9
Victor Grau Serrat <lacabra@users.noreply.github.com>
2019-02-14 23:28:53 -0600

    BLD: adds ppal node, fix drone, fix travis

--- enigma-core/start_principal.bash
+++ enigma-core/start_principal.bash
@@ -1,9 +1,15 @@
 #!/bin/bash
+contract=$(getent hosts enigma_contract_1 | awk '{ print $1 }')
+
+echo "Waiting for contracts to be deployed..."
+until curl -s -m 1 contract:8081 >/dev/null 2>&1; do sleep 1; done
+
 /opt/intel/libsgx-enclave-common/aesm/aesm_service &
 sleep 5 # give time to aesm_service to start
 
-contract=$(getent hosts enigma_contract_1 | awk '{ print $1 }')
 sed -i "s_http://[localhost|.0-9]*:9545_http://$contract:9545_" principal_test_config.json
+
+sed -i "s_5aeda56215b167893e80b4fe645ba6d5bab767de_1df62f291b2e969fb0849d99d9ce41e2f137006e_" /root/enigma-core/enigma-principal/app/tests/principal_node/config/deploy_config.json
 
 cp principal_test_config.json /root/enigma-core/enigma-principal/app/tests/principal_node/config
 
