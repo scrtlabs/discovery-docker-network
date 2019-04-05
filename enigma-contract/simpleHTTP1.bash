4319cb4548b4fd0c6da108288d68c5eb4d2e3cd3
Victor Grau Serrat <lacabra@users.noreply.github.com>
2019-04-05 16:26:51 -0500

    BLD: adjusted contracts addresses location to .engima

--- enigma-contract/simpleHTTP1.bash
+++ enigma-contract/simpleHTTP1.bash
@@ -1,4 +1,4 @@
 while true; do 
-	echo -ne "HTTP/1.1 200 OK\r\nContent-Length:$(wc -c < ~/enigma-contract/enigmacontract.txt)\r\nAccess-Control-Allow-Origin: *\r\n\r\n$(cat ~/enigma-contract/enigmacontract.txt)" | nc -l -p 8081 -q 1
+	echo -ne "HTTP/1.1 200 OK\r\nContent-Length:$(wc -c < ~/.enigma/enigmacontract.txt)\r\nAccess-Control-Allow-Origin: *\r\n\r\n$(cat ~/.enigma/enigmacontract.txt)" | nc -l -p 8081 -q 1
 done
 
